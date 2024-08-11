efc6dce499c4de7438ea96c29a0233da82b43425
junioralive <thejunioralive@gmail.com>
2024-08-11 12:27:40 +0530

    Auto Update fixes

--- spycli/utils/core/update.py
+++ spycli/utils/core/update.py
@@ -1,7 +1,13 @@
+import os
 import requests
 import subprocess
 import sys
 from packaging import version
+
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+version_file_path = os.path.join(base_dir, 'spycli', '__version__.py')
+
 from spycli.__version__ import __version__
 
 def get_latest_release_info():
@@ -29,19 +35,20 @@ def check_for_updates():
                 break
         if download_url and file_name:
             print("\r[...] Downloading the latest version...", end="", flush=True)
-            update_package(download_url, file_name)
+            update_package(download_url, file_name, latest_version)
         else:
             print("\r[!] No suitable update file found. Please check the release assets manually.")
     else:
         print("\r[✔] You are already using the latest version of SPYCLI.")
 
-def update_package(url, file_name):
+def update_package(url, file_name, new_version):
     try:
         response = requests.get(url)
         with open(file_name, "wb") as f:
             f.write(response.content)
         print("\r[...] Installing the latest version...", end="", flush=True)
         subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", file_name])
+        update_version_file(new_version)
         print("\r[✔] SPYCLI has been successfully updated to the latest version.")
         print("\r[!] Please restart the application to apply the updates.")
         sys.exit(0)
@@ -51,3 +58,8 @@ def update_package(url, file_name):
     except subprocess.CalledProcessError as e:
         print(f"\r[!] Error during installation: {e}")
         sys.exit(1)
+
+def update_version_file(new_version):
+    with open(version_file_path, 'w') as version_file:
+        version_file.write(f'__version__ = "{new_version}"\n')
+        print("\r[✔] Version file updated.")
